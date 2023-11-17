e758cb12da135faa9b3173b071c5dd8989dea00f
renier1989 <renier1989@gmail.com>
2023-11-17 17:31:08 -0500

    minor update showing a shop icon when a product is the order

--- components/ModalProduct.jsx
+++ components/ModalProduct.jsx
@@ -4,20 +4,20 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 
 export const ModalProduct = () => {
-  const { product, handleChangeModal, handleAddToOrder,order } = useKiosk();
+  const { product, handleChangeModal, handleAddToOrder, order } = useKiosk();
   const [quantity, setQuantity] = useState(1);
   const [edit, setEdit] = useState(false);
 
-  useEffect(()=>{
-    if(order.some(orderState => orderState.id === product.id))
-    {
-        const productInOrder = order.find(orderState => orderState.id === product.id);
-        setEdit(true)
-        setQuantity(productInOrder.quantity)
+  useEffect(() => {
+    if (order.some((orderState) => orderState.id === product.id)) {
+      const productInOrder = order.find(
+        (orderState) => orderState.id === product.id
+      );
+      setEdit(true);
+      setQuantity(productInOrder.quantity);
     }
-  },[order,product])
+  }, [order, product]);
 
-  
   return (
     <div className="md:flex gap-10">
       <div className="md:w-1/3">
@@ -100,14 +100,33 @@ export const ModalProduct = () => {
           </button>
         </div>
 
-        <button
-          className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
-          onClick={() => handleAddToOrder({ ...product, quantity })}
-        >
+        <div className="flex items-center justify-start gap-3">
+          <button
+            className="bg-indigo-600 hover:bg-indigo-800 px-5 py-2 mt-5 text-white font-bold uppercase rounded"
+            onClick={() => handleAddToOrder({ ...product, quantity })}
+          >
+            {!edit ? "Add to the Order" : "Save Changes"}
+          </button>
 
-            {!edit ? 'Add to the Order' : 'Save Changes'}
-          
-        </button>
+          {edit && (
+            <div>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-8 h-8 mt-4 text-indigo-800"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
