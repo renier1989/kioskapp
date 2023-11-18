7dbe489082ee9f892e22dcfccc46ee49a5ef8893
Renier Vargas <renier1989@gmail.com>
2023-11-18 09:58:08 -0500

    389 - Progess bar with Tailwind

--- components/Steps.jsx
+++ components/Steps.jsx
@@ -1,4 +1,5 @@
-import { useRouter } from "next/router"
+import useKiosk from "@/hooks/useKiosk";
+import { useRouter } from "next/router";
 const steps = [
   { step: 1, name: "Menu", url: "/" },
   { step: 2, name: "Summary", url: "/summary" },
@@ -6,16 +7,45 @@ const steps = [
 ];
 
 export const Steps = () => {
-  const route = useRouter()
+  const { handleChangeStep, step } = useKiosk();
+  console.log(step);
+  const route = useRouter();
+
+  const progressBar = () =>{
+    let value;
+    if(step === 1 ){
+      value = 2
+    }else if(step === 2){
+      value = 50
+    }else{
+      value = 100
+    } 
+
+    return value;
+  }
+
+  console.log(progressBar());
   return (
-    <div className="flex justify-between mb-5">
-      {steps.map((step) => (
-        <button 
-        onClick={()=>{
-          route.push(step.url)
-        }}
-        className="text-3xl font-bold" key={step.step}>{step.name}</button>
-      ))}
-    </div>
+    <>
+      <div className="flex justify-between mb-5">
+        {steps.map((step) => (
+          <button
+            onClick={() => {
+              route.push(step.url);
+              handleChangeStep(step.step);
+            }}
+            className="text-3xl font-bold"
+            key={step.step}
+          >
+            {step.name}
+          </button>
+        ))}
+      </div>
+      <div className="bg-gray-100 mb-10">
+        <div className={`rounded-full bg-amber-500 text-xs leading-none h-2 text-center text-white`} style={{
+          width : `${progressBar()}%`
+        }}></div>
+      </div>
+    </>
   );
 };
--- context/KioskProvider.jsx
+++ context/KioskProvider.jsx
@@ -6,10 +6,11 @@ const { createContext, useState, useEffect } = require("react");
 const KioskContext = createContext();
 const KioskProvider = ({ children }) => {
   const [categories, setCategories] = useState([]);
-  const [currentCategory, setCurrentCategory] = useState({})
-  const [product, setProduct] = useState({})
-  const [modal ,setModal] = useState(false)
-  const [order, setOrder] = useState([])
+  const [currentCategory, setCurrentCategory] = useState({});
+  const [product, setProduct] = useState({});
+  const [modal, setModal] = useState(false);
+  const [order, setOrder] = useState([]);
+  const [step, setStep] = useState(1);
 
   const getCategories = async () => {
     const { data } = await axios(`/api/categories`);
@@ -24,32 +25,36 @@ const KioskProvider = ({ children }) => {
     setCurrentCategory(categories[0]);
   }, [categories]);
 
+  const handleClickCategory = (id) => {
+    const category = categories.filter((cat) => cat.id === id);
+    setCurrentCategory(category[0]);
+  };
 
-  const handleClickCategory = id =>{
-    const category = categories.filter(cat=>cat.id === id);
-    setCurrentCategory(category[0])
-  }
-
-  const handleSetProduct = product =>{
+  const handleSetProduct = (product) => {
     setProduct(product);
-  }
-  const handleChangeModal = () =>{
+  };
+  const handleChangeModal = () => {
     setModal(!modal);
-  }
+  };
 
-  const handleAddToOrder = ({categoryId, image,...productQ}) =>{
-
-    if(order.some(orderP => orderP.id === productQ.id)){
+  const handleAddToOrder = ({ categoryId, image, ...productQ }) => {
+    if (order.some((orderP) => orderP.id === productQ.id)) {
       // if already exists the product in the order, I Update the product id the quantity changed
-      const productQUpdated = order.map(orderP => orderP.id === productQ.id ? productQ : orderP)
+      const productQUpdated = order.map((orderP) =>
+        orderP.id === productQ.id ? productQ : orderP
+      );
       setOrder(productQUpdated);
-      toast.success('Order Updated! 😉');
-    }else{
+      toast.success("Order Updated! 😉");
+    } else {
       // I add the product with the quantity to the order
-      setOrder([...order, productQ])
-      toast.success('Added to your order! 😁');
+      setOrder([...order, productQ]);
+      toast.success("Added to your order! 😁");
     }
     setModal(false);
+  };
+
+  const handleChangeStep = step =>{
+    setStep(step);
   }
 
   return (
@@ -63,7 +68,9 @@ const KioskProvider = ({ children }) => {
         modal,
         handleChangeModal,
         handleAddToOrder,
-        order
+        order,
+        step,
+        handleChangeStep
       }}
     >
       {children}
