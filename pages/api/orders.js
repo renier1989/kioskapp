c570feee54da9caf53cb951502176b5f6d971ed8
Renier Vargas <renier1989@gmail.com>
2023-11-21 11:42:39 -0500

    402 - inserting data in DB

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,6 +1,19 @@
+import { PrismaClient } from "@prisma/client";
+
 export default async function handler(req, res) {
+    const prisma = new PrismaClient();
+
   if (req.method === "POST") {
-    console.log(req.body);
-    return res.status(200).json({ method: "POST..." });
+    const {total, order, name, date} = req.body
+    
+    const orderDB = await prisma.order.create({
+        data:{
+            name,
+            total,
+            date,
+            order 
+        }
+    })
+        return res.status(200).json({orderDB});
   }
 }
