41953cc1e086be86a51ce8e511d5db756929ff7a
Renier Vargas <renier1989@gmail.com>
2023-11-21 13:08:59 -0500

    406 - creating the endpoing to get the orders

--- pages/api/orders.js
+++ pages/api/orders.js
@@ -1,19 +1,29 @@
 import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
-    const prisma = new PrismaClient();
+  const prisma = new PrismaClient();
 
+  // SECTION TO GET THE ORDERS
+  const orders = await prisma.order.findMany({
+    where:{
+        state:  false
+    }
+  })
+
+  res.status(200).json(orders);
+
+  // SECTION TO CREATE ORDERS
   if (req.method === "POST") {
-    const {total, order, name, date} = req.body
-    
+    const { total, order, name, date } = req.body;
+
     const orderDB = await prisma.order.create({
-        data:{
-            name,
-            total,
-            date,
-            order 
-        }
-    })
-        return res.status(200).json({orderDB});
+      data: {
+        name,
+        total,
+        date,
+        order,
+      },
+    });
+    res.status(200).json({ orderDB });
   }
 }
